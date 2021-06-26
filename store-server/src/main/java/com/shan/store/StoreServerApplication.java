e34b1d4e2afad8cab0f99b4e91794c514dd69b5e
shanbingwei <shanbingwei@cvte.com>
2021-06-26 10:10:58 +0800

    [init] 初始化

---
+++ store-server/src/main/java/com/shan/store/StoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.shan.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreServerApplication.class, args);
+    }
+
+}
